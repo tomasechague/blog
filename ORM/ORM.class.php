ac7ce56371910877b29c5041bd460bfc5d878ac4
tomasechague <tomas.lp@live.com>
2016-10-05 10:02:56 -0300

    ORM

--- ORM/ORM.class.php
+++ ORM/ORM.class.php
@@ -8,52 +8,59 @@
 require_once __DIR__ . '/../config/database.php';
 
 class ORM {
+
     protected $tableName;
     protected $fields;
 
-
-    public function save(){
-       if($this->getId()){
-           $this->update();
-       }else{
-           $this->insert();
-       }
-       return $this;
-        
+    public function save() {
+        if ($this->getId()) {
+            $this->update();
+        } else {
+            $this->insert();
+        }
+        return $this;
     }
-    public function delete(){echo 'delete';}
-   
-    private function update(){echo 'update';}
-    private function insert(){
-        echo $this->camel2dashed('UserValidator');
+
+    public function delete() {
+        echo 'delete';
+    }
+
+    private function update() {
+        echo 'update';
+    }
+
+    private function insert() {
+
+        $fields = implode(',', $this->fields);
+        $values = $this->getValues();
         
-       $fields = implode(',',$this->fields);
-       $values =  $this->getValues();
-        
+
         $pdo = conectar();
-        
+
         $statement = $pdo->prepare('INSER INTO :tableName(:fields)
                                     VALUES(:values)');
         $statement->bindParam(':tableName', $this->tableName);
         $statement->bindParam(':fields', $fields);
-        $statement->bindParam(':values',$values);
-        
-       // $statement->execute();
-        
+        $statement->bindParam(':values', $values);
+
+        // $statement->execute();
     }
-    
-    public function getId(){
+
+    public function getId() {
         throw new Exception('Este metodo debe estar implementado en la subclase');
     }
-    
-    public function getValues(){
-       $objectValues =  get_object_vars($this);
+
+    public function getValues() {
+        $objectValues = get_object_vars($this);
+        $values = [];
+        foreach ($this->fields as $field) {
+            $values[] = $objectValues[$field];
+        }
+        return $values;
     }
-    
+
     public function camel2dashed($className) {
-    return strtolower(preg_replace('/([a-zA-Z])(?=[A-Z])/', '$1_', $className));
-}
+        return strtolower(preg_replace('/([a-zA-Z])(?=[A-Z])/', '$1_', $className));
+    }
 
-
-    
 }
